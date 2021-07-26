9bcee994e6ecfcd13b5aecc89fd7e31fa2d762b0
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-07-27 01:57:57 +0800

    v 0.1.6

---
+++ NodePackageModule/app.js
@@ -0,0 +1,6 @@
+//const math = require('./math');
+const PI =  require('./math').PI;
+//console.dir(require('./math'));
+console.dir(require('./math').PI);
+//console.dir(PI);
+//console.dir(add(1, 2));
---
+++ NodePackageModule/math.js
@@ -0,0 +1,6 @@
+// module.exports.PI = 3.14;
+// module.exports.add = (x, y) => x + y;
+
+//exports.PI = 3.14159;
+//module.exports = {name: "kw"};
+exports.PI = 3.14159;
