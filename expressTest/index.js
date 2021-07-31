50910d9d1006479233c2abdc2ce46bb6b0395f0a
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-07-31 18:29:04 +0800

    v 0.2.0

--- expressTest/index.js
+++ expressTest/index.js
@@ -2,12 +2,27 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.get('/', (req, res) => {
+    res.send('home page');
+});
+
+app.get('/search', (req, res) => {
+    console.log(req.query);
+    res.send("haha");
+});
+
+app.get('*', (req, res) => {
+    res.send('404 not found.');
+});
+
 app.listen(port, ()=> {
     console.log(`listening on port: ${port}`);
 });
 
+/*
 app.use((req, res)=> {
     console.log('new quest');
     console.dir(req);
     res.send('hello!');
 });
+*/
