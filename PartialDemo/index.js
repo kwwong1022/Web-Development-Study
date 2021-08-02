c89e93717bfdbef3c76699a84d3206f915b92f4a
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-08-02 22:22:04 +0800

    v 0.2.2

---
+++ PartialDemo/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const redditData = require('./data.json');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+
+    if (data) {
+        res.render('subreddit.ejs', { ...data });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
