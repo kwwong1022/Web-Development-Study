c1cc3d1b97fa1b32823e48e54b61af8e53ff1454
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-08-22 10:24:18 +0800

    v 0.3.2

---
+++ MongooseBasic/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose
+    .connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connection open');
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+const personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+});
+
+personSchema.virtual('fullName').get(function () {
+    return `${this.first} ${this.last}`;
+});
+
+personSchema.pre('save', function() {
+    console.log('about to save');
+ })
+ 
+ personSchema.post('save', function() {
+    console.log('just saved');
+ }) 
+
+const Person = mongoose.model('Person', personSchema);
