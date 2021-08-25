198f7a9554fd52748219a02e120ae3883e39416d
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-08-25 20:00:11 +0800

    v 0.3.9

---
+++ YelpCamp/01 CampgroundsCRUD/seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+
+mongoose
+    .connect('mongodb://localhost:27017/yelp-camp-01', {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('database connected');
+});
+
+const sample = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const camp = new Campground({
+            title: sample(places),
+            price: 2.99,
+            description: sample(descriptors),
+            location: `${sample(cities).city}, ${sample(cities).state}`
+        })
+        console.log(camp);
+        // await camp.save();
+    }
+}
+
+seedDB();
